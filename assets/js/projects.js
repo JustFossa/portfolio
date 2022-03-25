e3e5bb08d45b26ee8e6c4c69a25a22ab059e85a8
JustFossa <panko.marek@yahoo.com>
2022-03-25 22:11:35 +0100

    small update

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,13 +1,8 @@
 const projects = [
     {
-        "name": "UniChat",
-        "description": "An online chatting website which has all facebook messagner functions.",
-        "url": "https://vlad-chat-app.netlify.app/"
-    },
-    {
-        "name": "Google forms to discord",
-        "description": "A simple script that sends google forms results to a discord webhook.",
-        "url": "https://github.com/xVlad44/Google_forms-to-Discord"
-    },
+        "name": "Design Wizard",
+        "description": "An npm package which helps you with web designing",
+        "url": "https://www.npmjs.com/package/design-wizard"
+    }
 
 ]
