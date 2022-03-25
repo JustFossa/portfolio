82c7d752ab7f65228f91e806899a7b15631df849
JustFossa <panko.marek@yahoo.com>
2022-03-25 22:04:16 +0100

    init

---
+++ assets/js/projects.js
@@ -0,0 +1,13 @@
+const projects = [
+    {
+        "name": "UniChat",
+        "description": "An online chatting website which has all facebook messagner functions.",
+        "url": "https://vlad-chat-app.netlify.app/"
+    },
+    {
+        "name": "Google forms to discord",
+        "description": "A simple script that sends google forms results to a discord webhook.",
+        "url": "https://github.com/xVlad44/Google_forms-to-Discord"
+    },
+
+]
