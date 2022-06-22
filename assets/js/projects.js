ab27cf5650515056238dfe08b3e0f10a273aae19
フォサー <panko.marek@icloud.com>
2022-06-22 22:36:47 +0200

    Update projects.js

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -3,6 +3,11 @@ const projects = [
         "name": "Design Wizard",
         "description": "An npm package which helps you with web designing",
         "url": "https://www.npmjs.com/package/design-wizard"
+    },
+    {
+        "name": "Lunch Finder",
+        "description" : "School project for finding pics of lunch... design definitely inspired by discord :)",
+        "url":"https://lunchfinder.justfossa.xyz"
     }
 
 ]
